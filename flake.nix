ff6bcba0aee6042d737344e8ec054627f2b9d3ea
Xiangyi Meng <xymeng16@gmail.com>
2024-10-06 21:22:54 +0800

    minimized config

--- flake.nix
+++ flake.nix
@@ -1,43 +1,37 @@
 {
-  description = "Xiangyi's nix configuration";
+  description = "A simple NixOS flake";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # 因此请将下面的 my-nixos 替换成你的主机名称
     nixosConfigurations = {
-      # FIXME replace with your hostname
-      "8086k" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
-      };
-    };
+      tortuga = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # 这里导入之前我们使用的 configuration.nix，
+          # 这样旧的配置文件仍然能生效
+          ./configuration.nix
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "xiangyi@8086k" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.xiangyi = import ./home.nix;
+            # home-manager.extraSpecialArgs = inputs;
+          }
+        ];
       };
     };
   };
